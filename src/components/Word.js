ef89240c599bc289f08e92135b1a1fbe18fd4c58
devdio <devdio.kei@gmail.com>
2024-01-09 00:29:50 +0900

    add

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
-function Word({word}) {
-    const [isShow, setIsShow] = React.useState(false);
-    const [isDone, setIsDone] = React.useState(word.isdone);
+function Word({word : w}) {
+    const [word, setWord] = useState(w);
+    const [isShow, setIsShow] = useState(false);
+    const [isDone, setIsDone] = useState(word.isdone);
 
     function toggleIsShow() {
         setIsShow(!isShow);
     }
 
     function toggleIsDone() {
-        setIsDone(!isDone);
+        // setIsDone(!isDone);
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                ...word,
+                isdone: !isDone,
+            }),
+            }).then(res => {
+            if (res.ok) {
+                setIsDone(!isDone);
+            }
+        });
     }
+
+    function deleteWord() {
+        if (window.confirm("삭제 하시겠습니까?")) {
+          fetch(`http://localhost:3001/words/${word.id}`, {
+            method: "DELETE",
+          }).then(res => {
+            if (res.ok) {
+              setWord({
+                ...word,
+                id: 0,
+              });
+            }
+          });
+        }
+      }
+    
+      if (word.id === 0) {
+        return null;
+      }
 
   return (
     <tr className={isDone? 'off': ''}>
@@ -21,7 +55,7 @@ function Word({word}) {
         <td>{isShow && word.ko}</td>
         <td>
             <button onClick={ toggleIsShow }>뜻 {isShow?'숨기기':'보기'}</button>
-            <button className="btn_del">삭제</button>
+            <button className="btn_del" onClick={deleteWord}>삭제</button>
         </td>
     </tr>
   )
