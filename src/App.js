56acef9277ec35561683f59eb4334c3c25cd1273
devdio <iamtopaz@gmail.com>
2024-01-08 16:15:32 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 
+import React from 'react'
+// import {useState} from 'react'
 import Hello from './components/Hello'
 import Welcome from './components/Welcome'
 
 function App() {
   
-  function showName() {
-    console.log('이름 보기')
-  }
-
-  function showText(val) {
-    console.log(val)
-  }
 
   return (
     <div className="App">
-      <button onClick={showName}>이름 보기</button>
-      <button onClick={ () => {
-        console.log('나이 : ', 30)
-      } }>나이 보기</button>
-      
-      <input type='text' onChange={
-        (e) => {
-          showText(e.target.value)
-        }
-      } />
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -3,14 +3,31 @@ import World from './World'
 import styles from './Hello.module.css'
 
 function Hello() {
-    console.log(styles)
+    // 배열을 반환 (구조 분해 문법)
+  const [name, setName] = React.useState('John');
+
+  function showText(val) {
+  }
+
+  function changeName() {
+    const newName = name == 'John' ? "Mike" : "John";
+    console.log(newName);
+    setName(newName);
+  }
 
   return (
-    <div>
-      <p className={styles.hello_world}>Hello!</p>
-      <World />
+    <div className="App">
+      <div>{name}</div>
+      <input type='text' onChange={
+        (e) => {
+          showText(e.target.value)
+        }
+      } />
+      <button onClick={ changeName }>이름 바꾸기</button>
+      
+      
     </div>
-  )
+  );
 }
 
 export default Hello
